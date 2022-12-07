4e2f36476781f0dce105ad0644db6e8dc6330c33
Önder Şahin <ndrshn@gmail.com>
2022-12-07 17:34:03 +0300

    update viewer page

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -25,26 +25,31 @@
           </a-card>
         </a-col>
         <a-col :span="24">
-          <a-card :title="`${year} Depremleri (${data.length} deprem)`" size="small">
-            <template #extra>
-              <a-select
-                v-model:value="year"
-                style="width: 120px"
-                size="small"
-                @change="loadData"
-              >
-                <a-select-option v-for="y in years" :key="y" :value="y">{{
-                  y
-                }}</a-select-option>
-              </a-select>
-            </template>
+          <a-card size="small">
             <a-table
               :data-source="data"
               :columns="columns"
               bordered
+              :loading="loading"
               :pagination="{ pageSize: 100 }"
               size="small"
-            />
+            >
+              <template #title>
+                <a-space>
+                  <a-select
+                    v-model:value="year"
+                    style="width: 100%"
+                    size="small"
+                    @change="loadData"
+                  >
+                    <a-select-option v-for="y in years" :key="y" :value="y">{{
+                      y
+                    }}</a-select-option>
+                  </a-select>
+                  <span>{{ data.length }} earthquakes</span>
+                </a-space>
+              </template>
+            </a-table>
           </a-card>
         </a-col>
       </a-row>
@@ -53,7 +58,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import {
   Table as ATable,
   Card as ACard,
@@ -61,6 +66,7 @@ import {
   SelectOption as ASelectOption,
   Col as ACol,
   Row as ARow,
+  Space as ASpace,
 } from "ant-design-vue";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -87,12 +93,22 @@ const years = [
   2021,
   2022,
 ];
-
 const year = ref(2003);
 const map = ref();
 const data = ref([]);
 const loading = ref(false);
-
+const levelLayers = reactive({
+  "M0.0-M0.9": L.layerGroup([]),
+  "M1.0-M1.9": L.layerGroup([]),
+  "M2.0-M2.9": L.layerGroup([]),
+  "M3.0-M3.9": L.layerGroup([]),
+  "M4.0-M4.9": L.layerGroup([]),
+  "M5.0-M5.9": L.layerGroup([]),
+  "M6.0-M6.9": L.layerGroup([]),
+  "M7.0-M7.9": L.layerGroup([]),
+});
+const layerControl = ref();
+const location = ref("");
 const columns = [
   {
     title: "id",
@@ -100,6 +116,11 @@ const columns = [
     dataIndex: "id",
     align: "center",
     width: 100,
+  },
+  {
+    title: "Date",
+    key: "date",
+    dataIndex: "date",
   },
   {
     title: "Location",
@@ -132,33 +153,81 @@ const columns = [
   },
 ];
 
+const getColor = (mag) => {
+  if (mag >= 0.0 && mag < 3) return 175;
+  else if (mag >= 3.0 && mag < 4.0) return 100;
+  else if (mag >= 4.0 && mag < 5.0) return 75;
+  else if (mag >= 5.0 && mag < 6.0) return 50;
+  else if (mag >= 6.0 && mag < 7.0) return 25;
+  else return 0;
+};
+
+const findRange = (mag) => {
+  return `M${Math.floor(mag).toFixed(1)}-M${(Math.floor(mag) + 0.9).toFixed(1)}`;
+};
+
 const loadData = async (value) => {
   loading.value = true;
   await import(`../data/${value}.json`).then((res) => {
     data.value = res.default;
   });
-  loading.value = false;
-
+  Object.keys(levelLayers).forEach((el) => {
+    levelLayers[el].clearLayers();
+  });
   data.value.forEach((el) => {
     const point = L.circle([el.lat, el.lng], {
-      color: `hsl(${(8 - el.mag) * 180}, 100%, 50%)`,
-      fillOpacity: 0.9,
-      radius: 200 * el.mag,
-    }).addTo(map.value);
+      color: `hsl(${getColor(el.mag)} 100% 50%)`,
+      fillOpacity: 0.8,
+      radius: 50 * el.mag * el.mag,
+      zIndexOffset: Math.ceil(el.mag) * 10,
+    });
     point.bindPopup(`${el.location}: M${el.mag} @ ${el.date}`);
+    levelLayers[findRange(el.mag)].addLayer(point);
   });
+  Object.keys(levelLayers).forEach((el) => {
+    layerControl.value.removeLayer(levelLayers[el]);
+    layerControl.value.addOverlay(levelLayers[el], el);
+  });
+  loading.value = false;
 };
+
 loadData(year.value);
+
+const initLegend = () => {
+  var legend = L.control({ position: "bottomleft" });
+  legend.onAdd = function (map) {
+    const list = L.DomUtil.create("ul", "legend");
+    list.className = "legends";
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(175 100% 50%)"></i><span>M0.0-2.9</span></li>`;
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(100 100% 50%)"></i><span>M3.0-3.9</span></li>`;
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(75 100% 50%)"></i><span>M4.0-4.9</span></li>`;
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(50 100% 50%)"></i><span>M5.0-5.9</span></li>`;
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(25 100% 50%)"></i><span>M6.0-6.9</span></li>`;
+    list.innerHTML += `<li class="legend"><i style="background-color: hsl(0 100% 50%)"></i><span>M7.0-7.9</span></li>`;
+    return list;
+  };
+  legend.addTo(map.value);
+};
+
 onMounted(() => {
   map.value = L.map("map").setView([38.9637, 35.2433], 6);
-  L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-    attribution: "...",
-    maxZoom: 18,
-  }).addTo(map.value);
+  L.tileLayer(
+    "https://cartodb-basemaps-b.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png",
+    {
+      tileset_id: "mapbox.mapbox-streets-v8,mapbox.mapbox-terrain-v2",
+      attribution: "...",
+      maxZoom: 20,
+    }
+  ).addTo(map.value);
+  layerControl.value = L.control.layers({}, {}).addTo(map.value);
+  Object.keys(levelLayers).forEach((el) => {
+    map.value.addLayer(levelLayers[el]);
+  });
+  initLegend();
 });
 </script>
 
-<style scoped>
+<style>
 .container {
   padding: 0;
   margin: 0;
@@ -266,5 +335,53 @@ onMounted(() => {
 #map {
   width: 100%;
   height: 640px;
+}
+
+.legends {
+  padding: 0 6px;
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+}
+
+.legends .legend {
+  margin-bottom: 3px;
+  font-size: 14px;
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
+}
+
+.legends span {
+  color: #eee;
+}
+
+.legends i {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  float: left;
+  opacity: 0.7;
+}
+
+#map,
+.ant-card,
+.ant-input,
+.ant-select:not(.ant-select-customize-input) .ant-select-selector,
+.ant-pagination-item {
+  border-radius: 5px;
+}
+
+.ant-input-search
+  > .ant-input-group
+  > .ant-input-group-addon:last-child
+  .ant-input-search-button {
+  border-radius: 0 5px 5px 0;
+}
+
+.ant-table-title {
+  border-radius: 5px 5px 0 0;
 }
 </style>
