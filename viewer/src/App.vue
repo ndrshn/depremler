46ef502dda787757383d9976546fc153f04a6112
Önder Şahin <ndrshn@gmail.com>
2022-12-08 08:49:25 +0300

    add title to header

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -16,6 +16,7 @@
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M3 12h4l3 8l4 -16l3 8h4" />
       </svg>
+      <span>Earthquakes in Türkiye</span>
     </header>
     <section class="content">
       <a-row :gutter="[24, 24]">
